01a408cbce868f076fb1708ca3e7a6012ce67fad
omaimaqasim <omaimaakhan15@gmail.com>
2026-04-18 11:50:41 +0500

    make responsove

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+       screens: {
+        'max-440': { 'max': '440px' },
+      }
+    },
   },
   plugins: [],
 }
